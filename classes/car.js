622afdd07a83efec19efae61cfb1a51cb6d59dbf
tonykle <tonyl3@uw.edu>
2016-03-04 00:31:51 -0800

    another class for prac

---
+++ classes/car.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// === My awesome car class ===
+class Car {
+  constructor(args) {
+    this.args = args;
+  }
+
+  // returns value of class property
+  get(prop) {
+    return this.args[prop];
+  }
+
+  // sets property of class
+  set(prop, val) {
+    this.args[prop] = val;
+  }
+
+  // checks if property of class has a value
+  has(prop) {
+    return (this.args[prop] != null && this.args[prop] != undefined);
+  }
+
+  // sets property of class to null
+  unset(prop) {
+    this.args[prop] = null;
+  }
+
+  printDetails() {
+    for (let i in this.args) {
+      console.log(i + " = " + this.args[i]);
+    }
+  }
+}
+
+var Acura = new Car({brand : 'Acura', year : 1999, color : 'white'});
+console.log(Acura.get('brand')); // should log Acura
+Acura.set('wheels', 20);
+console.log(Acura.get('wheels')); // should log 20
+
+if (Acura.has('color')) {
+  console.log('Acura has a set color'); // should log
+}
+
+Acura.unset('year');
+console.log(Acura.year); // should be undefined
+Acura.printDetails(); // should log: brand = Acura, year = null, color = white, wheels = 20
